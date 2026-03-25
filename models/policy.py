5178773a455d41643f3c1dde57d9bceaff6486ec
Andreas Holck Høeg-Petersen <anhh@cs.aau.dk>
2026-03-25 11:11:55 +0100

    evaluate and print result after training

--- models/policy.py
+++ models/policy.py
@@ -2,8 +2,10 @@ import numpy as np
 
 from stable_baselines3.ppo import PPO
 from stable_baselines3.common.callbacks import BaseCallback, CallbackList
+from stable_baselines3.common.evaluation import evaluate_policy
 from sb3_contrib.common.maskable.policies import MaskableActorCriticPolicy
 from sb3_contrib.ppo_mask import MaskablePPO
+from models.callbacks import DataLoggerCallback
 
 
 def init_model(env, model_type=PPO, **model_kwargs):
@@ -25,6 +27,9 @@ def train_model(model, n_timesteps, path=None, replay_path=None, callbacks=[], p
     callback = CallbackList(callbacks)
 
     model.learn(n_timesteps, callback=callback, progress_bar=progress_bar)
+    rew, std = evaluate_policy(model, model.env, n_eval_episodes=25)
+    print(rew, std)
+
     if replay_path is not None:
         np.save(replay_path, np.array(data_logger.data))
 
@@ -39,7 +44,7 @@ def load_or_train_model(
         replay_path='', continue_training=False, callbacks=[], **model_kwargs
 ):
     try:
-        model = model_type.load(path, device='cpu')
+        model = model_type.load(path, device='cpu', **model_kwargs)
         model.set_env(env)
     except FileNotFoundError:
         model = init_model(env, model_type=model_type, **model_kwargs)
